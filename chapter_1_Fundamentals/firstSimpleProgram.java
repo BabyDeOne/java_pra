192908b3fb2ba737b68375f34e4aa96346ccba2b
Liu <hanquanl0115@gmail.com>
2022-06-26 18:45:57 -0400

    20220626

---
+++ chapter_1_Fundamentals/firstSimpleProgram.java
@@ -0,0 +1,10 @@
+/*
+* This is a simple Java program.
+* Call this file firstSimpleProgram.java.
+* */
+
+class firstSimpleProgram{
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
