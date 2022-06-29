6000ee96ba01a01056769dd7a83c9c9ad1619b00
playerone <hanquanl0115@gmail.com>
2022-06-29 15:03:33 -0400

    20220629

---
+++ chapter_2_DataType&Operators/DynInit.java
@@ -0,0 +1,9 @@
+public class DynInit {
+    public static void main(String[] args){
+        double r = 4, h = 5;
+
+        double v = 3.1416 * r * r * h;
+
+        System.out.println("Volume is " + v);
+    }
+}
---
+++ chapter_2_DataType&Operators/StrDemo.java
@@ -0,0 +1,7 @@
+public class StrDemo {
+    public static void main(String[] args){
+        System.out.println("first line \nsecond line");
+        System.out.println("A \t B \t C");
+        System.out.println("D \t E \t F");
+    }
+}
