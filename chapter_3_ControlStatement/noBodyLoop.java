c35b91fa858abedf4c790c31fc999787ab1be976
playerone <hanquanl0115@gmail.com>
2022-07-08 21:11:16 -0400

    20220708

---
+++ chapter_3_ControlStatement/noBodyLoop.java
@@ -0,0 +1,8 @@
+public class noBodyLoop {
+    public static void main(String[] args){
+        int i, sum = 0;
+        for(i = 1; i <= 5; sum += i++){
+            System.out.println("sum is "+ sum);
+        }
+    }
+}
